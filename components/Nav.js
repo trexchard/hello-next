18115d43f3aab8e0a5ff55c020682e98952c78c0
Caleb Runion <calebrunion@icloud.com>
2021-09-14 20:51:55 -0400

    Nav: Create opacity toggle

--- components/Nav.js
+++ components/Nav.js
@@ -1,13 +1,32 @@
 import Link from 'next/link'
+import { useState } from 'react'
 
-const Nav = () => (
-  <nav className='order-first w-full p-3 mx-auto overflow-x-auto text-center sm:order-none sm:w-auto'>
-    <Link href='/'>Home</Link>
-    <span className='mx-3'>•</span>
-    <Link href='/weather'>Weather</Link>
-    <span className='mx-3'>•</span>
-    <Link href='/counter'>Counter</Link>
-  </nav>
-)
+const Nav = () => {
+  const [open, setOpen] = useState(false)
+
+  return (
+    <>
+      <div className='absolute inset-x-0 flex justify-center -mt-12'>
+        <button
+          onClick={() => setOpen(!open)}
+          className={`shadow-md rounded-lg p-2 select-none transition backdrop-blur-md bg-purple-200 bg-opacity-20 hover:bg-opacity-30`}
+        >
+          {open ? 'hide' : 'show'} nav
+        </button>
+      </div>
+      <nav
+        className={`order-first w-full ${
+          open ? '' : 'opacity-0 pointer-events-none'
+        } p-3 mx-auto overflow-x-auto transition text-center sm:order-none sm:w-auto`}
+      >
+        <Link href='/'>Home</Link>
+        <span className='mx-3'>•</span>
+        <Link href='/weather'>Weather</Link>
+        <span className='mx-3'>•</span>
+        <Link href='/counter'>Counter</Link>
+      </nav>
+    </>
+  )
+}
 
 export default Nav
