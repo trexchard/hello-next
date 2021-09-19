852da62ebbc8615a8c5d5b2c99953ee24c2ac41c
Caleb Runion <calebrunion@icloud.com>
2021-09-19 10:23:54 -0400

    GET first post from JSON Placeholder api

--- pages/blog.js
+++ pages/blog.js
@@ -1,6 +1,19 @@
 import Head from 'next/head'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
 
-export default function Weather() {
+const baseURL = 'https://jsonplaceholder.typicode.com/posts/1'
+
+export default function Blog() {
+  const [post, setPost] = useState(null)
+
+  useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      setPost(response.data)
+    })
+  })
+
+  if (!post) return null
   return (
     <div className='grid gap-4 text-center'>
       <Head>
