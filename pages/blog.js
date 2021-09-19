3e45f9153252ba04930c945afa5203bde179737a
Caleb Runion <calebrunion@icloud.com>
2021-09-19 09:58:41 -0400

    Template blog page

---
+++ pages/blog.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+
+export default function Weather() {
+  return (
+    <div className='grid gap-4 text-center'>
+      <Head>
+        <title>Blog - Caleb Runion</title>
+      </Head>
+    </div>
+  )
+}
